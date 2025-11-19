5f1647d9555d72c750a72e550557c9e654eed91d
Prajwal T <73645971+PrajwalGitCode@users.noreply.github.com>
2025-11-19 22:15:54 +0530

    hosting

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"], // your Netlify frontend
+  origin: ["https://monumental-banoffee-shopping.netlify.app/login"], // your Netlify frontend
   credentials: true
 }));
 app.use(express.json());
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://shoppingapp-a3kt.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
