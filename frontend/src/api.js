14043c26add15d6a3655abad1aabfde1cc014316
Prajwal T <73645971+PrajwalGitCode@users.noreply.github.com>
2025-11-19 22:47:53 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-const API_URL = "https://shoppingapp-a3kt.onrender.com";
+const API_URL = "https://shoppingapp-a3kt.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
